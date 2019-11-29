884c995e9cfce92c95846638c3f6a4037b89d408
Fran P <sweetydesigns@gmail.com>
2019-11-29 18:27:10 +0100

    Revert "piccolo: Add power HAL extension to disable big cluster"
    This reverts commit b1e42bf26ea450ccdde43c2a64496e92906f15c7.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -42,9 +42,6 @@ TARGET_TAP_TO_WAKE_NODE := "/sys/devices/soc.0/78b9000.i2c/i2c-5/5-004a/en_gestu
 # Filesystem
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 13013734522 # (BOARD_USERDATAIMAGE_PARTITION_SIZE - 16384 for crypto footer)
 
-# Power
-TARGET_POWERHAL_SET_INTERACTIVE_EXT := $(DEVICE_PATH)/power/power_ext.c
-
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 
