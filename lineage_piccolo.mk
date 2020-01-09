6e5371cf55cc3d02d828b15082a36f573bacc58b
Fran P <sweetydesigns@gmail.com>
2020-01-09 16:44:53 +0100

    piccolo: Drop old bootanimation configs

--- lineage_piccolo.mk
+++ lineage_piccolo.mk
@@ -1,9 +1,6 @@
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
-TARGET_BOOTANIMATION_HALF_RES := true
-TARGET_BOOTANIMATION_PRELOAD := true
-TARGET_BOOTANIMATION_TEXTURE_CACHE := true
 
 # Inherit some common Lineage stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
