5a794a9499ec7bcbed9e7404ab8c3ff849eed065
Fran Palmero <sweetydesigns@gmail.com>
2020-04-29 21:00:33 +0200

    piccolo: Move Display & Surfaceflinger props to common

--- device.mk
+++ device.mk
@@ -95,17 +95,6 @@ PRODUCT_PACKAGES += \
     init.target.rc \
     init.recovery.qcom.rc
 
-# SurfaceFlinger
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.surface_flinger.protected_contents=true \
-    ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
-    ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000 \
-    ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
-    ro.surface_flinger.start_graphics_allocator_service=true \
-    ro.surface_flinger.use_smart_90_for_video=true \
-    ro.surface_flinger.set_display_power_timer_ms=10000 \
-    ro.surface_flinger.set_touch_timer_ms=500	
-
 # Thermal
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:system/vendor/etc/thermal-engine.conf
